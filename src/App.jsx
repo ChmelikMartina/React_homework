fab340894bef4c883e95e843acfdaf099cf22b42
ChmelikMartina <brozova-m@seznam.cz>
2024-02-15 15:30:55 +0100

    second col done

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,28 @@ import Select from "./components/Select";
 import Range from "./components/Range";
 import Clock from "./components/Clock";
 import ProgressBar from "./components/ProgressBar";
+import TextBox from "./components/TextBox";
+import Button from "./components/Button";
+import TextArea from "./components/TextArea";
+import File from "./components/File";
+import saveText from "./functions/saveText";
 
 function App() {
   const scoopMin = 1;
   const scoopMax = 4;
-  const [initialFloat, setInitialFloat] = useState(0);
+  const initCountDown = 100;
+  const [initialFloat, setInitialFloat] = useState(10.1);
   const [flavor, setFlavor] = useState("vanilla");
   const [sprinkles, setSprinkles] = useState([]);
   const [scoop, setScoop] = useState(1);
   const iceCreamTypes = ["creamy", "yogurt", "low-fat"];
   const [iceCreamType, seticeCreamType] = useState("creamy");
   const [diskSpace, setDiskSpace] = useState(50);
+  const [countDown, setCountDown] = useState(0);
+  const [num2, setNum2] = useState(0);
+  const [num1, setNum1] = useState(initialFloat);
+  let [sum, setSum] = useState("Enter a float number and press the sum button");
+  const [text, setText] = useState("");
 
   useEffect(() => {
     let temp = prompt("Set the float number.", 10.1);
@@ -27,6 +38,15 @@ function App() {
     }
     setInitialFloat(parseFloat(temp));
   }, []);
+
+  useEffect(() => {
+    if (countDown < initCountDown) {
+      const timer = setInterval(() => {
+        setCountDown(countDown + 1);
+      }, 1000);
+      return () => clearInterval(timer);
+    }
+  }, [countDown]);
 
   const handleData = (data, source) => {
     switch (source) {
@@ -52,6 +72,42 @@ function App() {
       }
       case "rng-disk": {
         setDiskSpace(data);
+        break;
+      }
+      case "txb-number1": {
+        setNum1(data);
+        break;
+      }
+      case "txb-number2": {
+        setNum2(data);
+        break;
+      }
+      case "txa-text": {
+        setText(data);
+        break;
+      }
+      case "file-load": {
+        setText(data);
+        break;
+      }
+      default:
+        break;
+    }
+  };
+
+  const handleEvent = (source) => {
+    switch (source) {
+      case "btn-sum": {
+        if (!validateFloat(num1) || !validateFloat(num2)) {
+          setSum("Value is NOT valid");
+        } else {
+          let calc = parseFloat(num1) + parseFloat(num2);
+          setSum(`${calc}`);
+        }
+        break;
+      }
+      case "btn-download": {
+        saveText(text);
         break;
       }
       default:
@@ -115,20 +171,59 @@ function App() {
             </p>
           </div>
           <div className="col-6">
-            {/* prgbar */}
-            {/* vypis */}
+            <ProgressBar id="pgb-progress" dataIn={countDown} />
+            <p>
+              Installation is in progress, please wait{" "}
+              {initCountDown - countDown} seconds
+            </p>
             <div className="row">
-              <div className="col-6"></div>
-              <div className="col-6"></div>
+              <div className="col-6">
+                <TextBox
+                  label="Number 1"
+                  id="txb-number1"
+                  handleData={handleData}
+                  dataIn={num1}
+                />
+              </div>
+              <div className="col-6">
+                <TextBox
+                  label="Number 2"
+                  id="txb-number2"
+                  handleData={handleData}
+                  dataIn={num2}
+                />
+              </div>
             </div>
             <div className="row">
-              <div className="col-6"></div>
-              <div className="col-6"></div>
+              <div className="col-6">
+                <Button label="Sum" id="btn-sum" handleEvent={handleEvent} />
+              </div>
+              <div className="col-6">
+                <p>{sum}</p>
+              </div>
             </div>
-            {/* textarea */}
+            <TextArea
+              id="txa-text"
+              label="Text oparetions"
+              handleData={handleData}
+              height={150}
+              dataIn={text}
+            />
             <div className="row">
-              <div className="col-6"></div>
-              <div className="col-6"></div>
+              <div className="col-6">
+                <File
+                  id="file-load"
+                  label="Load text from file"
+                  handleData={handleData}
+                />
+              </div>
+              <div className="col-6">
+                <Button
+                  id="btn-download"
+                  label="Download the text file"
+                  handleEvent={handleEvent}
+                />
+              </div>
             </div>
           </div>
         </div>
